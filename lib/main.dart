fe844aed755fbc93f46f407f60ad3a47d055d122
Kosmic Teal <69432458+kosmicteal@users.noreply.github.com>
2023-10-27 16:02:26 +0200

    Fixed app title

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'M08 - Hello World',
+      title: 'M08 - Layout',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
